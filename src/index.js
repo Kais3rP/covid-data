c8427e5bd163e1bee5b66255a8d74c0d444168c9
Kais3rP <cesare.polonara@gmail.com>
2020-12-13 13:15:26 +0100

    fixed some code, improved styling

--- src/index.js
+++ src/index.js
@@ -1,110 +1,124 @@
 import * as d3 from "d3";
 
-async function covidGraph (){
-   var covidDataSet = [];
+async function covidGraph() {
+   try {
+      let covidDataSet = [];
+      const res = await fetch('https://cors-anywhere.herokuapp.com/https://opendata.ecdc.europa.eu/covid19/casedistribution/json/', { headers: { 'Accept': 'application/json' } })
+      const data = await res.json()
+      const covidData = data.records
 
-const res = await fetch('https://cors-anywhere.herokuapp.com/https://opendata.ecdc.europa.eu/covid19/casedistribution/json/', {headers : { 'Accept':'application/json'} })
-  const data = await res.json()
-  const covidData = data.records
-//console.log(covidData)
-   
-  
-   for (let el of covidData) el.countryterritoryCode === "ITA" ? covidDataSet.push([el.dateRep,el.deaths,parseInt(el.month)]):null;
-    
-   covidDataSet = covidDataSet.reverse().filter(x=>x[2]!==12)
-   //console.log(covidDataSet)
-
-const w = 800;
-const h = 500;
-const padding = 30;
-const colWidth = Math.floor(w-5*padding)/(covidDataSet.length);
+      //Hide loader when data is received
+      document.getElementsByClassName('title')[0].style = "display: block"
+      document.getElementsByClassName('loader')[0].style = "display: none"
 
 
-const xScale = d3.scaleLinear()
-                 .domain([d3.min(covidDataSet, (d)=>d[2]),d3.max(covidDataSet, (d)=>d[2])])
-                 .range([2*padding, w-3*padding])
-                 
-                
-const yScale = d3.scaleLinear()
-                 .domain([0, d3.max(covidDataSet, (d)=>d[1])])
-                 .range([0,h-3*padding])
-                               
-const yScaleAxis = d3.scaleLinear()
-                 .domain([0, d3.max(covidDataSet, (d)=>d[1])])
-                 .range([h-3*padding,0])
-                
-                
+      for (let el of covidData) el.countryterritoryCode === "ITA" ? covidDataSet.push([el.dateRep, el.deaths, parseInt(el.month)]) : null;
+      covidDataSet = covidDataSet.reverse()
+      const color = "#3C403D"
+      const hoverColor = "#FF3B3F"
+
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+      const padding = 30;
+      const w = windowWidth ;
+      const h = windowHeight ;
+
+      const colWidth = Math.floor(w - 5 * padding) / (covidDataSet.length);
+
+//Reset all body content before a redraw
+document.querySelector(".graph").innerHTML = ""
+
+      const xScale = d3.scaleLinear()
+         .domain([d3.min(covidDataSet, (d) => d[2]), d3.max(covidDataSet, (d) => d[2])])
+         .range([ padding, w - padding])
+
+      console.log(d3.max(covidDataSet, (d) => d[1]));
+      const yScale = d3.scaleLinear()
+         .domain([0, d3.max(covidDataSet, (d) => d[1])])
+         .range([0, h - 3 * padding])
+console.log(w,h)
+      const yScaleAxis = d3.scaleLinear()
+         .domain([0, d3.max(covidDataSet, (d) => d[1])])
+         .range([h - 3 * padding, 0])
 
 
-var toolTip = d3.select(".graph")
-                .append("div")
-                .attr("class","tooltip")
-                .style("display","none")
 
-var svg = d3.select('.graph')
-            .append("svg")
-            .attr("class","graphic")
-            .attr("height", h - padding)
-            .attr("width", w-2*padding)
+      const toolTip = d3.select(".graph")
+         .append("div")
+         .attr("class", "tooltip")
+         .style("display", "none")
 
-            svg.selectAll("rect")
-               .data(covidDataSet)
-               .enter()
-               .append("rect")
-               .attr("width",colWidth)
-               .attr("height",(d)=>d[1]>0 ? yScale(d[1]) : 1)
-               .attr("x", (d,i)=> 2*padding+i*colWidth)
-               .attr("y", (d,i)=> h - yScale(d[1]) - 2*padding)
-               .attr("fill", "rgb(173, 173, 173)")
-               .on('mouseover', function(d,i){
-                  
-                   d3.select(this).attr("fill","rgb(49, 49, 49)");
-                   toolTip.transition().duration(200).style("display","inline");
-                   toolTip.text(()=>`${d[0]}: ${d[1]} deaths`)
-                          .style("top",`${h/2}px`)
-                          .style("left",`${i*colWidth}px`)
-                  })
-               .on('mouseout', function(){ 
-                  d3.select(this).attr("fill","rgb(173, 173, 173)");
-                  toolTip.transition().duration(200).style("display","none");
-            })
-           
-                  
+      const svg = d3.select('.graph')
+         .append("svg")
+         .attr("class", "graphic")
+         .attr("preserveAspectRatio", "xMinYMin meet")
+         .attr("viewBox", `0 0 ${w} ${h}`)
 
-               const xAxis = d3.axisBottom(xScale)
-                               .ticks(6)
-               const yAxis = d3.axisLeft(yScaleAxis);
 
-               svg.append("g")
-                  .attr("transform", "translate("+2*padding+","+padding+ ")")
-                  .call(yAxis);
+      svg.selectAll("rect")
+         .data(covidDataSet)
+         .enter()
+         .append("rect")
+         .attr("width", colWidth)
+         .attr("height", (d) => d[1] > 0 ? yScale(d[1]) : 1)
+         .attr("x", (d, i) => 2 * padding + i * colWidth)
+         .attr("y", (d, i) => h - yScale(d[1]) - 2 * padding)
+         .attr("fill", color)
+         .on('mouseover', function (d, i) {
 
-               svg.append("g")
-                  .attr("transform", "translate(0,"+(h-2*padding)+ ")")
-                  .call(xAxis);
- svg.append('text')
-    .attr('transform', 'rotate(-90)')
-    .attr('x', -h/2)
-    .attr('y', 90)
-    .text('Deaths of Covid - 19')
-    .attr('class', 'axes-text');
+            d3.select(this).attr("fill", hoverColor);
+            toolTip.transition().duration(200).style("display", "inline");
+            toolTip.text(() => `${d[0]}: ${d[1]} deaths`)
+               .style("top", `${h / 2}px`)
+               .style("left", `${i * colWidth}px`)
+         })
+         .on('mouseout', function () {
+            d3.select(this).attr("fill", color);
+            toolTip.transition().duration(200).style("display", "none");
+         })
 
- svg.append('text')
-    .attr('x', 70)
-    .attr('y', h- 70)
-    .text('Month')
-    .attr('class', 'axes-text');
-  
- svg.append('text')
-    .attr('x', padding*2)
-    .attr('y', h-padding)
-    .text('Data: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data')
-    .attr('class', 'info');
-    document.getElementsByClassName('title')[0].style = "display: block"
-    document.getElementsByClassName('loader')[0].style = "display: none"
+      const xAxis = d3.axisBottom(xScale)
+         .ticks(6)
+      const yAxis = d3.axisLeft(yScaleAxis);
+
+      svg.append("g")
+         .attr("transform", "translate(" + padding + "," + padding + ")")
+         .call(yAxis);
+
+      svg.append("g")
+         .attr("transform", "translate(0," + (h - 2 * padding) + ")")
+         .call(xAxis);
+      svg.append('text')
+         .attr('transform', 'rotate(-90)')
+         .attr('x', -h / 2)
+         .attr('y', 90)
+         .text('Deaths of Covid - 19')
+         .attr('class', 'axes-text');
+
+      svg.append('text')
+         .attr('x', 70)
+         .attr('y', h - 70)
+         .text('Month')
+         .attr('class', 'axes-text');
+
+      svg.append('text')
+         .attr('x', padding * 2)
+         .attr('y', h - padding)
+         .text('Data: https://data.europa.eu/euodp/en/data/dataset/covid-19-coronavirus-data')
+         .attr('class', 'info');
+   } catch (e) {
+      throw new Error(e)
    }
+}
 //Call the graph builder
-   covidGraph()
-   
+covidGraph().then(ok => console.log(ok)).catch(e => { console.log(e); document.body.innerText = "Something went wrong :-(" })
 
-   
+/*
+let redrawTimeout;
+window.onresize = () => {
+   clearTimeout(redrawTimeout);
+ redrawTimeout = setTimeout(()=> {
+   covidGraph().then(ok => console.log("resizing window, refetching and redrawing graph")).catch(e => { console.log(e); document.body.innerText = "Something went wrong :-(" })
+  },2000) 
+}
+*/
